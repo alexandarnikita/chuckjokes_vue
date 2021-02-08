1b762653c09b03caa2a990ba6c1c19e845fea385
Nikita Alexandar <alexandar.nikita@zoho.com>
2021-02-08 04:39:49 -0800

    add footer component

---
+++ src/components/Footer/AppFooter.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="app-footer">
+    <div class="container">
+      <div class="footer-action">
+        <p class="footer-submit-lead">Got Jokes? Get paid for submitting?</p>
+        <p class="footer-link">
+          submit joke
+          <img src="../../assets/images/arrow-right.png" alt="Link Icon" class="link-icon" />
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'AppFooter',
+    components: { },
+    props: {},
+    computed: {},
+    data () {
+      return {}
+    }
+  }
+
+</script>
+
+<style lang="scss">
+</style>
