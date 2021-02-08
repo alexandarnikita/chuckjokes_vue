a414e2ccf27ded6f0c98a604223531a03f6a932b
Nikita Alexandar <alexandar.nikita@zoho.com>
2021-02-08 02:12:39 -0800

    initialize project

---
+++ src/containers/HomeContainer.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="main-frame">
+    <app-header/>
+    <app-footer/>
+  </div>
+</template>
+
+<script>
+  import AppHeader from '../components/Header/AppHeader'
+  import AppFooter from '../components/Footer/AppFooter'
+
+  export default {
+    name: 'Home',
+    components: {
+      AppHeader,
+      AppFooter
+    },
+    props: {},
+    computed: {},
+    data () {
+      return {}
+    }
+  }
+
+</script>
+
+<style lang="scss" scoped>
+  .main-frame {
+    width: 100%;
+    height: 100%;
+  }
+</style>
